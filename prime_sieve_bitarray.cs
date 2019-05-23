494f80e2b1aa4932cb47664e4bc609697bad8d5b
old-grimnir <49686783+old-grimnir@users.noreply.github.com>
2019-05-23 15:36:52 +0100

    optimised (array is faster)

--- prime_sieve_bitarray.cs
+++ prime_sieve_bitarray.cs
@@ -12,27 +12,41 @@ namespace Scratch2name
             // using a bit array (again, not good for huge numbers)
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start(); 
-            int num = 15485863;
+            long num = 15485863;
             //int num = 13;
-            int range = num + 1;
-            BitArray pl = new BitArray(range);
+            long range = num + 1;
+            BitArray pl = new BitArray((int)range);
 
             pl.SetAll(true);
 
             pl[0] = false; // 0 not prime
             pl[1] = false; // 1 not prime
 
-            for (int j = 2; j < range; j++) // start at 2
+            for (long j = 2; j < range; j++) // start at 2
             {
-                if (pl[j]) // look for 'true'
+                if (pl[(int)j]) // look for 'true'
                 {
-                    for (int k = j + j; k < range; k += j)
+
+                    if ( j <= 10)
                     {
-                        pl[k] = false; // mark out all multiples
+                        for (long k = j * j; k < range; k += j)
+                        {
+                            pl[(int)k] = false; // mark out all multiples
+                        }
                     }
+                    else
+                    {
+                        if ( j % 2 != 0 || j % 3 != 0 || j % 5 != 0 || j % 7 != 0)
+                        {
+                            for (long k = j + j; k < range; k += j)
+                            {
+                                pl[(int)k] = false; // mark out all multiples   
+                            }
+                        }
+                    }                    
                 }
             }
-            Console.WriteLine("Is prime: {0}", pl[num]);
+            Console.WriteLine("Is prime: {0}", pl[(int)num]);
             Console.WriteLine("Took: {0} ticks", watch.ElapsedTicks);
             Console.WriteLine("Took: {0} ms", watch.ElapsedMilliseconds);
         }
